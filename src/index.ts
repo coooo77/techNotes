619bd770f999425284560c36e41d6a97e1c796f0
william chang <coooo77@hotmail.com>
2023-04-03 23:34:02 +0800

    Chapter 1: MERN Stack Project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('hello world')
+import path from 'path'
+import express from 'express'
+import root from './routes/root'
+
+const app = express()
+
+const PORT = process.env.PORT || 3500
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', root)
+
+app.all('*', (req, res) => {
+  res.status(404)
+
+  // 對應 request 所設定 header 的類型
+  const accepts = req.accepts(['html', 'json'])
+  switch (accepts) {
+    case 'html':
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+      break
+    case 'json':
+      res.json({ message: '404 Not Found' })
+      break
+    default:
+      res.type('txt').send('404 Not Found')
+      break
+  }
+})
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
